1e168329b02757b8d6c5dac8ed8f5830ef0d7595
Alberto García <albert111.garcia@gmail.com>
2024-08-25 17:26:43 +0200

    Update dim_reduction.py

--- how/layers/dim_reduction.py
+++ how/layers/dim_reduction.py
@@ -5,6 +5,35 @@ from torch import nn
 
 from ..utils import whitening
 
+class LinearDimReduction(nn.Linear):
+    """Dimensionality reduction as a linear layer
+
+    :param int input_dim: Network out_channels
+    :param in dim: Whitening out_channels, for dimensionality reduction
+    """
+
+    def __init__(self, input_dim, dim):
+        super().__init__(input_dim, dim, bias=True)
+
+    def initialize_pca_whitening(self, des):
+        """Initialize PCA whitening from given descriptors. Return tuple of shift and projection."""
+        m, P = whitening.pcawhitenlearn_shrinkage(des)
+        m, P = m.T, P.T
+
+        projection = torch.Tensor(P[:self.weight.shape[0], :])
+        self.weight.data = projection.to(self.weight.device)
+
+        projected_shift = -torch.mm(torch.FloatTensor(P), torch.FloatTensor(m)).squeeze()
+        self.bias.data = projected_shift[:self.weight.shape[0]].to(self.bias.device)
+        return m, P
+
+    def load_initialization(self, m, P):
+        """Load PCA whitening from given shift and projection."""
+        projection = torch.Tensor(P[:self.weight.shape[0], :])
+        self.weight.data = projection.to(self.weight.device)
+
+        projected_shift = -torch.mm(torch.FloatTensor(P), torch.FloatTensor(m)).squeeze()
+        self.bias.data = projected_shift[:self.weight.shape[0]].to(self.bias.device)
 
 class ConvDimReduction(nn.Conv2d):
     """Dimensionality reduction as a convolutional layer
@@ -14,7 +43,7 @@ class ConvDimReduction(nn.Conv2d):
     """
 
     def __init__(self, input_dim, dim):
-        super().__init__(input_dim, dim, (1, 1), padding=0, bias=True)
+        super().__init__(input_dim, dim, kernel_size=(1, 1), stride=(1, 1), padding=(0, 0), bias=True)
 
     def initialize_pca_whitening(self, des):
         """Initialize PCA whitening from given descriptors. Return tuple of shift and projection."""
@@ -26,4 +55,43 @@ class ConvDimReduction(nn.Conv2d):
 
         projected_shift = -torch.mm(torch.FloatTensor(P), torch.FloatTensor(m)).squeeze()
         self.bias.data = projected_shift[:self.weight.shape[0]].to(self.bias.device)
-        return m.T, P.T
+        return m, P
+
+    def load_initialization(self, m, P):
+        """Load PCA whitening from given shift and projection."""
+        projection = torch.Tensor(P[:self.weight.shape[0], :]).unsqueeze(-1).unsqueeze(-1)
+        self.weight.data = projection.to(self.weight.device)
+
+        projected_shift = -torch.mm(torch.FloatTensor(P), torch.FloatTensor(m)).squeeze()
+        self.bias.data = projected_shift[:self.weight.shape[0]].to(self.bias.device)
+
+
+class ConvDimReduction3D(nn.Conv3d):
+    """Dimensionality reduction as a convolutional layer
+
+    :param int input_dim: Network out_channels
+    :param in dim: Whitening out_channels, for dimensionality reduction
+    """
+
+    def __init__(self, input_dim, dim):
+        super().__init__(input_dim, dim, kernel_size=(1, 1, 1), stride=(1, 1, 1), padding=(0, 0, 0), bias=True)
+
+    def initialize_pca_whitening(self, des):
+        """Initialize PCA whitening from given descriptors. Return tuple of shift and projection."""
+        m, P = whitening.pcawhitenlearn_shrinkage(des)
+        m, P = m.T, P.T
+
+        projection = torch.Tensor(P[:self.weight.shape[0], :]).unsqueeze(-1).unsqueeze(-1).unsqueeze(-1)
+        self.weight.data = projection.to(self.weight.device)
+
+        projected_shift = -torch.mm(torch.FloatTensor(P), torch.FloatTensor(m)).squeeze()
+        self.bias.data = projected_shift[:self.weight.shape[0]].to(self.bias.device)
+        return m, P
+
+    def load_initialization(self, m, P):
+        """Load PCA whitening from given shift and projection."""
+        projection = torch.Tensor(P[:self.weight.shape[0], :]).unsqueeze(-1).unsqueeze(-1).unsqueeze(-1)
+        self.weight.data = projection.to(self.weight.device)
+
+        projected_shift = -torch.mm(torch.FloatTensor(P), torch.FloatTensor(m)).squeeze()
+        self.bias.data = projected_shift[:self.weight.shape[0]].to(self.bias.device)
